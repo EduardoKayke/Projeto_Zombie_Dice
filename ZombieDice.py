bb2a77e43f63a92d86b464a2b449d89ff13a0d8a
EduardoKayke <hakuzosu.developer@gmail.com>
2022-10-18 14:38:14 -0300

    Objetivos do jogo

--- ZombieDice.py
+++ ZombieDice.py
@@ -2,13 +2,19 @@
 Utilizou as estruturas condicionais (if, else e elif)​
 Utilizou os operadores lógicos e aritméticos ​
 Criou os  dados utilizando uma string (6 verdes, 4 amarelos e 3 vermelhos) ​
-Adicionou a entrada para receber o jogadores​
 Implementou a rotina para sortear os dados (selecionar três dados)
 Implementou a rotina para jogar os dados (sortear a face de um dos dados)
-Implementou a entrada para receber a quantidade de jogadores ​
-Efetua checagem se foi adicionado pelo menos dois jogadores​
 Criou as variáveis para contabilizar os tiros, cérebros e passos​
 Incrementa as variáveis quando um dos dados é lançado​
+Utilizou as estruturas repetição (for e while)
+Realiza a Interação do jogador utilizando a estrutura de repetição​
+Implementou a rotina para sortear os dados dentro da estrutura de repetição​
+Implementou a rotina para jogar os dados dentro da estrutura de repetição​
+Criou as variáveis para contabilizar os tiros, cérebros e passos adicionando na estrutura de repetição​
+Incrementa as variáveis quando um dos dados são lançados​
+Verifica o resultado dos três dados lançados​
+
+
 """
 print("----------------------------------------")
 print("--------Bem-Vindo ao Zombie Dice--------")
@@ -35,12 +41,27 @@ print("Qual é o nome dos Zombies?")
 
 nome_jogadores = []
 
-while nome_jogadores < len(jogadores):
+for nome_jogadores in len(jogadores):
     nome_jogadores = input(f"Zombie {nome_jogadores}. Qual seu nome?")
     
-#     Mostrar o número de jogadores
+
 # . Usuário deve informar o nome dos jogadores
 #     Mostrar o nome dos jogadores
+
+# Assim, você deverá criar os dados neste momento utilizando um objeto do tipo string. 
+# Esta string deve conter seis caracteres para simular cada face do dado. Para 
+# identificar as faces dos dados segue abaixo especificação:
+# 6 Dados verdes: “CPCTPC”
+# 4 Dados amarelos: “TPCTPC”
+# 3 Dados vermelhos: “TPTCPT”
+# Onde, o caractere “C” na string corresponde ao cérebro, caractere “P” são os passos e 
+# por fim o “T” é o tiro.
+# Neste momento você deve utilizar a string de forma aleatória, poderá utilizar a função 
+# random.choice() ou se preferir poderá trabalhar com o índice da string.
+# Assim, utilizando os dados, você já consegue começar a trabalhar com a lógica do jogo. 
+# Consegue definir blocos de código utilizando as estruturas condicionais, ou seja, 
+# quando o jogador cair em cada um das faces do dado, poderá incrementar um contador 
+# (cérebro, passos 
 
 # . Se o jogador comeu 13 cérebros.
 #     Mostrar o nome do jogador
